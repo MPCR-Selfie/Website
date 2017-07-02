b3717d55fe0bd0eeccfd65c54fa302d4fe998324
Simone Nathaniel <shedamoe@gmail.com>
2017-07-02 00:33:18 -0400

    fixed responsive images

--- packages/packages.js
+++ packages/packages.js
@@ -1,5 +1,7 @@
 var app = angular.module("selfieApp");
 
 app.controller("packagesController", ["$scope", function ($scope) {
-    
+    $scope.packageChoice = "";
+
+    $scope.autoMessage = $scope.packageChoice += "Hi! I am interested in booking your standard package"
 }])
